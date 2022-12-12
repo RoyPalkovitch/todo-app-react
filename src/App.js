cb3ce43fe6d4632e2aac0b3d30ed7f0405987a32
Roy Palkovitch <roy.palkovitch@gmail.com>
2022-12-12 17:54:19 +0200

    fix bugs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import { useRef, useState, useContext } from 'react';
+import {useState} from 'react';
 import './App.css';
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../src/comp/navBar";
 import { AuthContext } from './providers/auth-context';
-
+import { ScreenContext } from './providers/appscreen-context';
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
+  const [todosScreen, settodoScreens] = useState([]);
+
   return (
     <>
       <AuthContext.Provider value={{
         currentUser,
         setCurrentUser
       }}>
-        <Navbar />
-        <Outlet />
+        <ScreenContext.Provider value={{ todosScreen, settodoScreens }}>
+          <Navbar />
+          <Outlet />
+        </ScreenContext.Provider>
       </AuthContext.Provider>
     </>
   )
--- src/comp/footer.js
+++ src/comp/footer.js
@@ -1,18 +1,25 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { ListContext } from "../providers/list-context";
 
 export function Footer() {
   const {changeCurrentView, noneCompletedItemsCount, clearAllCompleted} = useContext(ListContext);
-
+  let navigate = useNavigate();
   function changeViewHandle(view){
     changeCurrentView(view);
+    view === 'All'?(
+      navigate('/todosApp')
+    ):( 
+      navigate('/todosApp/'+view.toLowerCase())
+      );
+ 
   }
 
   return (
     <footer className='footer'>
       <span className="todo-count"><strong>{noneCompletedItemsCount}</strong> items left</span>
       <button className="clear-completed" onClick={clearAllCompleted}>Clear completed</button>
-      <button className="clear-completed" onClick={() => changeViewHandle('All Tasks')}>All</button>
+      <button className="clear-completed" onClick={() => changeViewHandle('All')}>All</button>
       <button className="clear-completed" onClick={() => changeViewHandle('Active')}>Active</button>
       <button className="clear-completed" onClick={() => changeViewHandle('Completed')}>Completed</button>
     </footer>
